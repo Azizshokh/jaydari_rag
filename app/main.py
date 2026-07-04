2a0b0b634d181f38cb882a12166d65e6a0f5f35d
ali <erali.jurakulov@mail.ru>
2026-07-05 03:23:43 +0900

    BRR: initial configuration & health_check endpoint creation

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import logging
+
+from app.api.routes import router
+from app.core.config import Settings
+
+logger = logging.getLogger("uvicorn")
+logger.info("*** JAYDARI RAG STARTS ***")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = Settings()
+    app.state.settings = settings
+    yield
+
+
+app = FastAPI(
+    title='JAYDARI RAG',
+    version='1.0.0',
+    lifespan=lifespan
+)
+
+app.include_router(router)
